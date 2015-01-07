e334395552995efc90e4981b45a490e9c1ab7699
Matthieu de Canteloube <matts2cant@gmail.com>
2015-01-07 10:40:57 +0100

    Initial Commit

---
+++ ANSnapchatAPI/ANSnapchatAPI.h
@@ -0,0 +1,13 @@
+//
+//  ANSnapchatAPI.h
+//  ANSnapchatAPI
+//
+//  Created by Matthieu de Canteloube on 07/01/2015.
+//  Copyright (c) 2015 unixpickle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ANSnapchatAPI : NSObject
+
+@end
---
+++ ANSnapchatAPI/ANSnapchatAPI.m
@@ -0,0 +1,13 @@
+//
+//  ANSnapchatAPI.m
+//  ANSnapchatAPI
+//
+//  Created by Matthieu de Canteloube on 07/01/2015.
+//  Copyright (c) 2015 unixpickle. All rights reserved.
+//
+
+#import "ANSnapchatAPI.h"
+
+@implementation ANSnapchatAPI
+
+@end
